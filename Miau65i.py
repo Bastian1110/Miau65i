5807f4c1445d8e3a1b5682ec9c1de08c09aa3e37
Bastian1110 <chebimora02@gmail.comm>
2022-01-17 17:55:31 -0600

    Initial Support of Boolean Operators

--- Miau65i.py
+++ Miau65i.py
@@ -1,46 +1,56 @@
 import sys 
+import re
 
 ADDRESS_COUNT = 0x02
 RESERV_WORDS = ["if","else","while"]
+OPERATIONS = ['+','-']
+BOOLEAN_OP = ['<','>','==','!=']
+TYPES = ["int"]
 
-def checkAddresCount():
+def checkAddresCount(sum):
     global ADDRESS_COUNT
     if(ADDRESS_COUNT == 254):
         ADDRESS_COUNT = 512
     else:
-        ADDRESS_COUNT+=2
+        ADDRESS_COUNT+=sum
+
+
+class Allocation:
+    def getTable(type,scope):
+        out = {"ADDRESS" :  str(hex(ADDRESS_COUNT))[2:],"TYPE" : type, "SCOPE" : scope}
+        checkAddresCount(2)
+        return out
+
 
 class Equal:
-    def getTable(scope,arguments, optype):
-        out = {"SCOPE" : scope, "ADDRESS" : str(hex(ADDRESS_COUNT))[2:], "VALUE" : arguments, "OPTYPE" : optype}
-        checkAddresCount()
-        return out
-
-    def getAsm(table,data,name):
-        if(data["OPTYPE"] == "DECL"):
-            if(data["VALUE"][0].isdigit()):
-                msb = "{:04x}".format(int(data["VALUE"][0]))[:2]
-                lsb = "{:04x}".format(int(data["VALUE"][0]))[2:]
+    global OPERATIONS
+    def getAsm(table,data):
+        if(data[1] in table):
+            value = data[3:]
+            name = data[1]
+        else:
+            value = data[2:]
+            name = data[0]
+        if(len(value) == 1):
+            if(value[0].isdigit()):
+                msb = "{:04x}".format(int(value[0]))[:2]
+                lsb = "{:04x}".format(int(value[0]))[2:]
                 print("  lda #$" + msb)
-                print("  sta $" + data["ADDRESS"] + "      ; " + name)
+                print("  sta $" + table[name]["ADDRESS"] + "      ; " + name)
                 print("  lda #$" + lsb)
-                print("  sta $" + data["ADDRESS"] + " + 1\n")
+                print("  sta $" + table[name]["ADDRESS"] + " + 1\n")
             else:
-                print("  lda $" + table[data["VALUE"][0]]["ADDRESS"])
-                print("  sta $" + data["ADDRESS"] + "      ; " + name)
-                print("  lda $" + table[data["VALUE"][0]]["ADDRESS"] + " + 1")
-                print("  sta $" + data["ADDRESS"] + " + 1\n")
-        elif(data["OPTYPE"] == "MATH"):
-            if('+' in data["VALUE"]):
-                Plus.getAsm(table,data["VALUE"][0],data["VALUE"][2])
-                print("  sty $" + data["ADDRESS"] + "      ; " + name)
-                print("  sta $" + data["ADDRESS"] + " + 1\n")
+                print("  lda $" + table[value[0]]["ADDRESS"])
+                print("  sta $" + table[name]["ADDRESS"] + "      ; " + name)
+                print("  lda $" + table[value[0]]["ADDRESS"] + " + 1")
+                print("  sta $" + table[name]["ADDRESS"] + " + 1\n")
+        else:
+            if('+' in value):
+                Plus.getAsm(table,value[0],value[2])
+                print("  sty $" + table[name]["ADDRESS"] + "      ; " + data[1])
+                print("  sta $" + table[name]["ADDRESS"]+ " + 1\n")
 
 class Plus:
-    def getTable(scope,argumentOne,argumentTwo):
-        out = {"SCOPE" : scope, "ADDRESS" : str(hex(ADDRESS_COUNT)), "VALUEONE" : argumentOne,"VALUETWO": argumentTwo}
-        return out
-
     def getAsm(table,argumentOne,argumentTwo):
         typeP = [argumentOne.isdigit(),argumentTwo.isdigit()]
         if (not typeP[0] and not typeP[1]):
@@ -60,74 +70,105 @@ class Plus:
             print("  lda #$" + lsb)
             print("  adc $" + table[indirect]["ADDRESS"] + " + 1")
 
+class Boolean:
+    def getAsm(symbolsTable,statement):
+        global BOOLEAN_OP
+        statement = re.split('([^a-zA-Z0-9])',statement)
+        while ' ' in statement:
+            blank = statement.index(' ')
+            statement.pop(blank)
+        while '' in statement:
+            blank = statement.index('')
+            statement.pop(blank)
+        if(statement.count('=') == 2):
+            statement.pop(statement.index('='))
+            statement[1] = '=='
+        operator = statement[1]
+
+        if(operator == '<'):
+            print("Menor Que")
+        elif(operator == '>'):
+            print("  sec")
+            print("  lda $" + symbolsTable[statement[0]]["ADDRESS"])
+            print("  sbc $" + symbolsTable[statement[2]]["ADDRESS"])
+            print("  tay         ; " + statement[0] + " < " + statement[2])
+            print("  lda $" + symbolsTable[statement[0]]["ADDRESS"] + " + 1")
+            print("  sbc $" + symbolsTable[statement[2]]["ADDRESS"] + " + 1")
+
+
+        elif(operator == '=='):
+            print("IGUAL")
+
+        return statement
+       
+
+            
+
+
+
             
 class Iff:
-    def getTable(statement,beginIf,endIf,beginElse,endElse):
-        out = {"IFBEGIN":beginIf,"IFEND":endIf,"ELSEBEGIN":beginElse,"ELSEEND":endElse,"STATEMENT":statement}
-        return out
+    def getTable(statement,begin,end):
+        return {"IFBEGIN":begin,"IFEND":end,"STATEMENT":statement}
+    def getAsm(code,tableSym,tableFlow,labelCounter):
+        statement = Boolean.getAsm(tableSym,tableFlow["IF"+str(labelCounter)]["STATEMENT"])
+        if(statement[1] == '>'):
+            print("  bpl IF"+str(labelCounter))
+            print("")
+            print("ELSE0:")
 
-    def searchInfo(program):
-        out = [0,0,0,0] #IfBegin , IfEnd , ElseBegin, ElseEnd
-        stack = []
-        res = []
-        for l in range(len(program)):
-            if('{' in program[l]):
-                stack.append(program[l])
 
-            elif('}' in program[l]):
-                res.append([stack.pop(),l])
-        print(stack)
-        print(res)
-        stack = []
-        res = []
-        print("---------------")
-        for l in range(len(program)):
-            if('if' in program[l]):
-                stack.append(program[l])
+class Elsee:
+    def getTable(iff,begin,end):
+        return {"ELBEGIN":begin,"ELEND" : end,"IFKEY" : iff}
+    def getAsm(code,table):
+        Boolean.getAsm()
 
-            elif('else' in program[l]):
-                res.append([stack.pop(),l])
-        print(stack)
-        print(res)
-        return out
-                
+class Whilee:
+    def getTable(statement,begin,end):
+        return {"WHBEGIN":begin,"WHEND":end,"STATEMENT":statement}
+
 
 
 
             
 
 def createStateTable(code):
-    x = 0
     table = {}
-    Iff.searchInfo(code)
+    stack = []
+    blocksLimits = []
+    label = 0
+    ifCounter = []
+    for l in range(len(code)):
+        if('{' in code[l]):
+            stack.append(l)
+        elif('}' in code[l]):
+            blocksLimits.append([stack.pop(),l])
+    blocksLimits = sorted(blocksLimits, key=lambda x: x[0])
+    for i in blocksLimits:
+        if("if" in code[i[0]]):
+            table["IF"+str(label)] = Iff.getTable(code[i[0]][1],i[0],i[1])
+            if("else" in code[i[1]+1]):
+                ifCounter.append(label)
+            label+=1
+        elif("else" in code[i[0]]):
+            table["ELSE"+str(ifCounter[-1])] = Elsee.getTable("IF"+str(ifCounter[-1]),i[0],i[1])
+            ifCounter.pop()
+        elif("while" in code[i[0]]):
+            table["WHILE" + str(label)] = Whilee.getTable(code[i[0]][1],i[0],i[1])
+
+            
     return table
     
     
 
-
-
-def getOperand(line):
-    operands = ['=','+','-',"p"]
-    out = {}
-    for i in operands:
-        out[i] = []
-        x = 0
-        for w in line:
-            if(w == i):
-                out[i].append(x)
-            x+=1
-    return out
-
-
 def createSymbolTable(code):
+    global TYPES
     table = {}
     for l in code:
-        operands = getOperand(l)
-        if(len(operands['=']) != 0):
-            if(len(operands['+']) != 0 or len(operands['-']) != 0):
-                table[l[0]] = Equal.getTable(1,l[2:],"MATH")
-            else:
-                table[l[0]] = Equal.getTable(1,l[2:],"DECL")
+        for ty in TYPES:
+            if(ty in l):
+                table[l[1]] = Allocation.getTable(ty,1)
     return table
 
 
@@ -136,8 +177,6 @@ def handleKeyWords(line):
     banned = ['(',')']
     out = []
     word = ""
-    
-
     for i in line:
         if(i not in banned):
             word = word + i
@@ -160,7 +199,7 @@ def readFile(filePath):
     for x in range(len(program)):
         reservFlag = not(any(check in program[x] for check in RESERV_WORDS))
         if reservFlag:
-            program[x] = program[x].split(' ')
+            program[x] = re.split('([^a-zA-Z0-9])',program[x])
             while(' ' in program[x]):
                 blank = program[x].index(' ')
                 program[x].pop(blank)
@@ -171,9 +210,18 @@ def readFile(filePath):
             program[x] = handleKeyWords(program[x])
     return program
 
-def parseCode(code,table):
+def parseCode(code,tableS,tableC):
+    labelCounter = 0
     for l in code:
-        pass
+        if('=' in l):
+            Equal.getAsm(tableS,l)
+
+        elif("if" in l):
+            Iff.getAsm(code,tableS,tableC,labelCounter)
+            labelCounter+=1
+            
+        elif("else" in l):
+            print("Aqui va un Else")
         
 
 
@@ -182,16 +230,16 @@ def assemblyCode():
     program = readFile("foo.miau")
     symTable = createSymbolTable(program)
     stateTable = createStateTable(program)
+    parseCode(program,symTable,stateTable)
 
-    parseCode(program,symTable)
-#    for i in symTable:
-#        print(i,symTable[i])
+    for i in stateTable:
+        print(i,stateTable[i])
 
 
 
 
 if __name__ == "__main__":
-    #TODO #1  Create a proper command line for the compiler
+    #TODO #2 Create a proper command line for the compiler
     assemblyCode()
 
 

25fc0150bf219af24bd755c385d9f185d9ae9413
Seb Mora <sebmora@Sebs-MacBook-Pro.local>
2022-08-08 16:18:58 -0500

    Attempt to implement Lexer

--- lib/lexer.go
+++ lib/lexer.go
@@ -24,45 +24,43 @@ func Lexer() {
 
 func getToken() string {
 	fmt.Println("---------")
-	fmt.Println("Actual Position :", pos)
-	fmt.Println("Actual Buffer :", Buffer[pos:])
-	if pos >= len(Buffer) {
+	fmt.Println("Actual Buffer :", Buffer)
+	if len(Buffer) < 0 {
 		return "1"
 	} else {
-		fmt.Println("Buffer Inside : ", Buffer[pos:])
 		space := regexp.MustCompile(`\S`).FindStringIndex(Buffer[pos:])
 
-		if len(space) != 0 {
-			fmt.Println("Space Match : ", space)
-			pos = space[0]
-		} else {
+		if len(space) == 0 {
 			return "1"
+		} else if space[0] != 0 {
+			Buffer = Buffer[space[0]:]
+		} else {
+			Buffer = Buffer[0:]
 		}
 
 		for token, exp := range regex {
-			fmt.Println("Pos", pos, "buffer : ", Buffer[pos:])
-			match := exp.FindStringIndex(Buffer[pos:])
+			fmt.Println("Atemmpt : ", token)
+			match := exp.FindStringIndex(Buffer)
 			if len(match) != 0 {
+				fmt.Println("Token Faund !")
 				fmt.Println(Buffer[match[0]:match[1]], token)
-				pos = match[1] + 1
+				Buffer = Buffer[match[1]:]
 				return "0"
 			}
+			fmt.Println("Token Not Faund")
 		}
 		return "1"
 	}
 }
 
 func Test() {
-	Buffer = "11 a 4 5"
-	x := 0
-	for {
-		state := getToken()
-		if state == "1" {
-			break
-		}
-		x++
-		if x == 3 {
-			break
-		}
-	}
+	re := regexp.MustCompile("(?P<string>[a-zA-Z]+)|(?P<int>[0-9]+)")
+	input := "aa 1"
+	groupNames := re.SubexpNames()
+	fmt.Println(groupNames)
+	firstMatch := re.FindStringIndex(input)
+	fmt.Println(input[firstMatch[0]:firstMatch[1]])
+	input = input[firstMatch[1]:]
+	secondMatch := re.FindStringIndex(input)
+	fmt.Println(input[secondMatch[0]:secondMatch[1]])
 }
